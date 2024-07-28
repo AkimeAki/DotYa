f6c2243f2a9378cf8e14124acfa7260053ddab13
Aki <62400162+AkimeAki@users.noreply.github.com>
2024-07-28 23:51:55 +0900

    ドット絵カスタマイズ機能実装

---
+++ src/components/molecules/Modal.tsx
@@ -0,0 +1,109 @@
+/** @jsxImportSource @emotion/react */
+
+import { css } from "@emotion/react";
+
+interface Props {
+	isOpen: boolean;
+	children: React.ReactNode;
+	setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function ({ isOpen, setIsOpen, children }: Props) {
+	return (
+		<div
+			css={css`
+				user-select: none;
+				pointer-events: none;
+				opacity: 0;
+
+				${isOpen &&
+				css`
+					user-select: auto;
+					pointer-events: auto;
+					opacity: 1;
+				`}
+			`}
+		>
+			<div
+				onClick={() => {
+					setIsOpen(false);
+				}}
+				css={css`
+					position: fixed;
+					top: 120px;
+					right: 130px;
+					z-index: 1002;
+
+					&:after {
+						content: "";
+						display: block;
+						position: absolute;
+						top: 1px;
+						left: -13px;
+						background-color: #492b18;
+						width: 40px;
+						height: 40px;
+						z-index: 1002;
+						cursor: pointer;
+					}
+
+					@media (max-width: 990px) {
+						top: 70px;
+						right: 80px;
+					}
+
+					@media (max-width: 700px) {
+						top: 20px;
+						right: 30px;
+					}
+				`}
+			>
+				<span
+					css={css`
+						position: relative;
+						font-size: 30px;
+						font-weight: bold;
+						z-index: 1004;
+						color: white;
+						user-select: none;
+						pointer-events: none;
+					`}
+				>
+					×
+				</span>
+			</div>
+			<div
+				css={css`
+					position: fixed;
+					top: 50%;
+					left: 50%;
+					transform: translate(-50%, -50%);
+					width: calc(100% - 200px);
+					height: calc(100% - 200px);
+					background-color: rgba(235, 173, 126, 0.9);
+					border: 7px solid #492b18;
+					z-index: 1000;
+					overflow-y: scroll;
+
+					@media (max-width: 990px) {
+						width: calc(100% - 100px);
+						height: calc(100% - 100px);
+					}
+
+					@media (max-width: 700px) {
+						width: 100%;
+						height: 100%;
+					}
+				`}
+			>
+				<div
+					css={css`
+						margin-top: 60px;
+					`}
+				>
+					{children}
+				</div>
+			</div>
+		</div>
+	);
+}
---
+++ src/pages/custom/_page.tsx
@@ -0,0 +1,350 @@
+/** @jsxImportSource @emotion/react */
+import Button from "@/components/atoms/Button";
+import PictureFrame from "@/components/atoms/PictureFrame";
+import Title from "@/components/atoms/Title";
+import Modal from "@/components/molecules/Modal";
+import type { DotIllust } from "@/types";
+import { css } from "@emotion/react";
+import type { MicroCMSContentId, MicroCMSDate } from "microcms-js-sdk";
+import { Fragment, useEffect, useState } from "react";
+
+interface Props {
+	dots: (DotIllust & MicroCMSContentId & MicroCMSDate)[];
+}
+
+export default function ({ dots }: Props): JSX.Element {
+	const [image1Path, setImage1Path] = useState<string>("/transparent.png");
+	const [image2Path, setImage2Path] = useState<string>("/transparent.png");
+	const [selectImage, setSelectImage] = useState<1 | 2>(1);
+	const [isOpenImage1, setIsOpenImage1] = useState<boolean>(false);
+	const [isOpenImage2, setIsOpenImage2] = useState<boolean>(false);
+
+	useEffect(() => {
+		document.body.dataset.selectImage = String(selectImage);
+	}, [selectImage]);
+
+	useEffect(() => {
+		const canvas = document.querySelector<HTMLCanvasElement>("#canvas");
+		if (canvas === null) {
+			return;
+		}
+
+		const context = canvas.getContext("2d");
+		if (context === null) {
+			return;
+		}
+
+		let imageLoaded = 0;
+		const imageData: {
+			image: HTMLImageElement;
+			src: string;
+			x: number;
+			y: number;
+			oldX: number;
+			oldY: number;
+			diffX: number;
+			diffY: number;
+		}[] = [];
+
+		imageData.push({ image: new Image(), src: image1Path, x: 0, y: 0, oldX: 0, oldY: 0, diffX: 0, diffY: 0 });
+		imageData.push({ image: new Image(), src: image2Path, x: 0, y: 0, oldX: 0, oldY: 0, diffX: 0, diffY: 0 });
+
+		for (let i = 0; i < imageData.length; i++) {
+			const data = imageData[i];
+			if (data !== undefined) {
+				data.image.addEventListener("load", () => {
+					imageLoaded++;
+				});
+				data.image.src = data.src;
+				data.image.crossOrigin = "anonymous";
+			}
+		}
+
+		let click = false;
+		let pointX = 0;
+		let pointY = 0;
+		let oldPointX = 0;
+		let oldPointY = 0;
+
+		const mouseMove = (e: MouseEvent) => {
+			pointX = e.offsetX;
+			pointY = e.offsetY;
+		};
+
+		const mouseDown = (e: MouseEvent) => {
+			click = true;
+			pointX = e.offsetX;
+			pointY = e.offsetY;
+			oldPointX = e.offsetX;
+			oldPointY = e.offsetY;
+			canvas.style.cursor = "grabbing";
+			document.body.style.userSelect = "none";
+		};
+
+		const mouseUp = () => {
+			click = false;
+			canvas.style.cursor = "grab";
+			document.body.style.userSelect = "";
+			for (let i = 0; i < imageData.length; i++) {
+				const data = imageData[i];
+				if (data !== undefined) {
+					data.oldX = data.x;
+					data.oldY = data.y;
+				}
+			}
+		};
+
+		window.addEventListener("mouseup", mouseUp);
+		canvas.addEventListener("mousemove", mouseMove);
+		canvas.addEventListener("mousedown", mouseDown);
+
+		let animationId: number;
+		const tick = () => {
+			if (click) {
+				const data = imageData[Number(document.body.dataset.selectImage) - 1];
+				if (data !== undefined) {
+					data.diffX = -1 * Math.ceil((64 * (oldPointX - pointX)) / canvas.offsetWidth);
+					data.diffY = -1 * Math.ceil((64 * (oldPointY - pointY)) / canvas.offsetHeight);
+					data.x = data.oldX + data.diffX;
+					data.y = data.oldY + data.diffY;
+				}
+			}
+
+			if (imageLoaded === imageData.length) {
+				context.clearRect(0, 0, 64, 64);
+				for (let i = 0; i < imageData.length; i++) {
+					const data = imageData[i];
+					if (data !== undefined) {
+						context.drawImage(data.image, data.x, data.y);
+					}
+				}
+
+				const data = imageData[Number(document.body.dataset.selectImage) - 1];
+				if (data !== undefined) {
+					context.drawImage(data.image, data.x, data.y);
+				}
+			}
+
+			animationId = window.requestAnimationFrame(tick);
+		};
+		tick();
+
+		return () => {
+			canvas.removeEventListener("mousemove", mouseMove);
+			canvas.removeEventListener("mousedown", mouseDown);
+			window.removeEventListener("mouseup", mouseUp);
+			window.cancelAnimationFrame(animationId);
+		};
+	}, [image1Path, image2Path]);
+
+	return (
+		<>
+			<div
+				css={css`
+					display: flex;
+					flex-direction: column;
+					gap: 30px;
+				`}
+			>
+				<Title>カスタマイズ</Title>
+				<p>
+					ここでは2つのドット絵をくっつけることができます！
+					<br />
+					お好みのカスタマイズをしてみてね！
+				</p>
+				<div
+					css={css`
+						display: flex;
+						gap: 20px;
+					`}
+				>
+					<div
+						css={css`
+							display: flex;
+							flex-direction: column;
+							gap: 30px;
+						`}
+					>
+						<div
+							css={css`
+								display: flex;
+								flex-direction: column;
+								gap: 10px;
+								align-items: flex-start;
+							`}
+						>
+							<div
+								css={css`
+									cursor: pointer;
+								`}
+								onClick={() => {
+									setSelectImage(1);
+								}}
+							>
+								<PictureFrame src={image1Path} size={128} alt="1つ目の画像" />
+							</div>
+							<Button
+								size="slim"
+								onClick={() => {
+									setIsOpenImage1(true);
+								}}
+							>
+								ドット絵を変更する
+							</Button>
+							<Button
+								size="slim"
+								onClick={() => {
+									setSelectImage(1);
+								}}
+							>
+								選択する
+							</Button>
+						</div>
+						<div
+							css={css`
+								display: flex;
+								flex-direction: column;
+								gap: 10px;
+								align-items: flex-start;
+							`}
+						>
+							<div
+								css={css`
+									cursor: pointer;
+								`}
+								onClick={() => {
+									setSelectImage(2);
+								}}
+							>
+								<PictureFrame src={image2Path} size={128} alt="2つ目の画像" />
+							</div>
+							<Button
+								size="slim"
+								onClick={() => {
+									setIsOpenImage2(true);
+								}}
+							>
+								ドット絵を変更する
+							</Button>
+							<Button
+								size="slim"
+								onClick={() => {
+									setSelectImage(2);
+								}}
+							>
+								選択する
+							</Button>
+						</div>
+					</div>
+					<div
+						css={css`
+							flex: 1;
+						`}
+					>
+						<canvas
+							id="canvas"
+							width={64}
+							height={64}
+							css={css`
+								width: 100%;
+								image-rendering: pixelated;
+								cursor: grab;
+								background-color: white;
+							`}
+						/>
+					</div>
+				</div>
+				<Button
+					onClick={() => {
+						const download = async () => {
+							const canvas = document.querySelector<HTMLCanvasElement>("#canvas");
+							if (canvas === null) {
+								return;
+							}
+
+							const dataURL = canvas.toDataURL("image/png");
+							const blob = await (await fetch(dataURL)).blob();
+							const link = document.createElement("a");
+							const blobUrl = URL.createObjectURL(blob);
+
+							link.href = blobUrl;
+							link.download = `image.png`;
+							link.click();
+
+							// @ts-ignore
+							dataLayer.push({ event: "download-custom" });
+						};
+
+						void download();
+					}}
+				>
+					カスタマイズしたドット絵をダウンロード
+				</Button>
+			</div>
+			<Modal isOpen={isOpenImage1} setIsOpen={setIsOpenImage1}>
+				<div
+					css={css`
+						padding: 20px;
+						display: flex;
+						flex-wrap: wrap;
+						gap: 20px;
+						justify-content: center;
+					`}
+				>
+					{dots.map((dot) => {
+						if (dot.dot32 !== undefined) {
+							return (
+								<div
+									key={dot.id}
+									css={css`
+										cursor: pointer;
+									`}
+									onClick={() => {
+										setImage1Path(dot.dot32?.url ?? "");
+										setIsOpenImage1(false);
+									}}
+								>
+									<PictureFrame src={dot.dot32.url} size={128} alt="2つ目の画像" />
+								</div>
+							);
+						}
+
+						return <Fragment key={dot.id}></Fragment>;
+					})}
+				</div>
+			</Modal>
+			<Modal isOpen={isOpenImage2} setIsOpen={setIsOpenImage2}>
+				<div
+					css={css`
+						padding: 20px;
+						display: flex;
+						flex-wrap: wrap;
+						gap: 20px;
+						justify-content: center;
+					`}
+				>
+					{dots.map((dot) => {
+						if (dot.dot32 !== undefined) {
+							return (
+								<div
+									key={dot.id}
+									css={css`
+										cursor: pointer;
+									`}
+									onClick={() => {
+										setImage2Path(dot.dot32?.url ?? "");
+										setIsOpenImage2(false);
+									}}
+								>
+									<PictureFrame src={dot.dot32.url} size={128} alt="2つ目の画像" />
+								</div>
+							);
+						}
+
+						return <Fragment key={dot.id}></Fragment>;
+					})}
+				</div>
+			</Modal>
+		</>
+	);
+}
