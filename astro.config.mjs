3e0139f42a7898bdfc8092ecb595642da834f357
Aki <62400162+AkimeAki@users.noreply.github.com>
2024-07-30 08:59:04 +0900

    メタ情報更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,29 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 
+const site = "https://pixel.gives";
+
 // https://astro.build/config
 export default defineConfig({
 	server: {
 		port: 9001,
 		host: "0.0.0.0"
 	},
-	site: "https://pixel.gives",
+	site,
 	trailingSlash: "never",
 	integrations: [
 		sitemap({
 			filter: (page) => {
-				return page !== "";
+				const pageArray = page.replace(`${site}/`, "").split("/");
+				if (pageArray[0] === "tags" && pageArray[2] === "page") {
+					return false;
+				}
+
+				if (pageArray[0] === "page" && Number(pageArray[1]) > 1) {
+					return false;
+				}
+
+				return true;
 			}
 		}),
 		react()
