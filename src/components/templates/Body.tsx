11acd279033f11adf7e3872116e84091af5bd0b4
Aki <62400162+AkimeAki@users.noreply.github.com>
2024-10-21 00:24:01 +0900

    高さ修正

--- src/components/templates/Body.tsx
+++ src/components/templates/Body.tsx
@@ -25,7 +25,6 @@ export default function ({ children, tags }: Props): JSX.Element {
 					css={css`
 						display: flex;
 						flex-direction: column;
-						height: 100%;
 					`}
 				>
 					<div
