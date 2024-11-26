04228cd7d6a5858c03d5d20539d4b135e86ebbd9
Aki <62400162+AkimeAki@users.noreply.github.com>
2024-11-27 05:09:50 +0900

    パンくず追加

--- src/define.ts
+++ src/define.ts
@@ -1,3 +1,4 @@
 export const spWidth = 1130;
 export const fixedDescription =
 	"「どっとや」はドット絵のフリーイラスト素材屋さんです。動物、植物、絵文字や食べ物など、いろいろなドット絵を揃えているので、ぜひいろいろなシーンでご利用ください。";
+export const serviceTitle = "どっとや";
