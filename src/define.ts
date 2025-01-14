0054924b747dc09704b0c5072b0bd2a598d88a72
Aki <62400162+AkimeAki@users.noreply.github.com>
2025-01-14 21:05:19 +0900

    言語リストに簡体字中国語を追加し、hrefLangリストを更新してリンク生成ロジックを改善

--- src/define.ts
+++ src/define.ts
@@ -1,4 +1,5 @@
 export const siteRootUrl = "https://pixel.gives";
-export type Lang = "ja" | "zh-cn" | "zh-tw";
+export type Lang = "ja" | "zh-cn" | "zh-tw" | "zh";
 export const langList: Lang[] = ["ja", "zh-tw", "zh-cn"];
 export const pageLangList: Lang[] = langList.filter((l) => l !== "ja");
+export const hrefLangList: Lang[] = ["ja", "zh-tw", "zh-cn", "zh"];
